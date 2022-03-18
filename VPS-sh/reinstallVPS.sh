60ad59322a6d13f549546a54bc257934f17e2c6f
Aromalct <34676548+Aromalct@users.noreply.github.com>
2022-03-18 15:10:41 +0800

    Add files via upload

--- VPS-sh/reinstallVPS.sh
+++ VPS-sh/reinstallVPS.sh
@@ -20,53 +20,65 @@ clear
 echo && echo -e "${Green_font} 欢迎使用VPS重装部署脚本 ${Font_suffix}
 
 ————————————系统相关————————————
- ${Green_font}1.${Font_suffix} 修改ssh密码
- ${Green_font}2.${Font_suffix} 修改ssh端口
- ${Green_font}3.${Font_suffix} 修改系统时区
- ${Green_font}4.${Font_suffix} 系统升级
- ${Green_font}5.${Font_suffix} 路由测试
- ${Green_font}6.${Font_suffix} 安装开启BBR
+ ${Green_font}1.${Font_suffix} 系统升级
+ ${Green_font}2.${Font_suffix} 修改时区
+ ${Green_font}3.${Font_suffix} 改ssh密码
+ ${Green_font}4.${Font_suffix} 改ssh端口
+ ${Green_font}5.${Font_suffix} 创建密钥对及设置
+ ${Green_font}6.${Font_suffix} 安装iptables-persistent工具并禁用25端口
+ ${Green_font}7.${Font_suffix} 路由测试
+ ${Green_font}8.${Font_suffix} 安装开启BBR
  
 ————————————功能安装————————————
- ${Green_font}7.${Font_suffix} 安装acme.sh
- ${Green_font}8.${Font_suffix} 安装caddy2
- ${Green_font}9.${Font_suffix} 安装V2-UI
- ${Green_font}10.${Font_suffix} 安装X-UI
+ ${Green_font}9.${Font_suffix} 安装acme.sh
+ ${Green_font}10.${Font_suffix} 安装caddy2
+ ${Green_font}11.${Font_suffix} 安装V2-UI
+ ${Green_font}12.${Font_suffix} 安装X-UI
+ ${Green_font}13.${Font_suffix} 安装Soga-V2Board
+ 
 ————————————————————————————————"
 echo
 read -p " 请输入数字 [1-10]:" function
 case "$function" in
 	1)
-	chang_ssh_password
-	;;
-	2)
-	chang_ssh_port
-	;;
-	3)
-	chang_time_zone
-	;;
-	4)
 	system_update
 	;;
+	2)
+	chang_time_zone
+	;;
+	3)
+	chang_ssh_password
+	;;
+	4)
+	chang_ssh_port
+	;;
 	5)
-	Routing_test
+	Create_key_pair
 	;;
 	6)
-	install_BBR
+	install_iptables_persistent
 	;;
 	7)
-	install_acme
+	Routing_test
 	;;
 	8)
-	install_caddy2
+	install_BBR
 	;;
 	9)
-	install_V2_UI
+	install_acme
 	;;
 	10)
+	install_caddy2
+	;;
+	11)
+	install_V2_UI
+	;;
+	12)
 	install_X_UI
 	;;
-		
+	13)
+	install_Soga_V2Board
+	;;	
 	*)
 	clear
 	echo -e "${Error}:请输入正确数字 [1-10]"
@@ -76,8 +88,143 @@ case "$function" in
 esac
 }
 
+install_Soga_V2Board(){
+    echo -e "${Info}:安装宝塔面板"
+	wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh
+	
+	echo -e "${Info}:请登录宝塔进行环境安装
+    #选择使用LNMP的环境安装方式勾选如下信息
+     Nginx 1.18.0
+     MySQL 5.6 （或者更高版本，根据vps性能选择安装）
+     PHP-7.4
+     PM2管理器
+    #选择 Fast 快速编译后进行安装。"
+	
+	echo -e "${Info}:在宝塔面板安装redis
+	#上述软件安装完成后，在 BTPanel面板 > 软件商店 > 找到PHP 7.4 > 点击设置 > 安装扩展 > 选择redis进行安装
+	#一定要确定redis安装完成，可能出现界面提示安装完成，但实际并没有安装的bug"
+
+	echo -e "${Info}:在宝塔面板解除被禁止的函数
+	在 BTPanel面板 > 软件商店 > 找到PHP 7.4 > 点击设置 > 禁用函数 中将 putenv proc_open pcntl_alarm pcntl_signal 从列表中删除
+	至此，所有依赖软件都安装完成，下面配置网站"
+	
+	echo -e "${Info}:在宝塔面板添加站点
+	#BTPanel面板 > 网站 > 添加站点
+	 在 域名 填入你指向服务器的域名
+	 在 数据库 选择MySQL
+	 在 数据库设置 填写数据库 用户名 密码
+	 在 PHP Verison 选择PHP-74"
+	
+	echo -e "${Info}:安装V2Board"
+	  #通过SSH登录到服务器后访问站点路径如：/www/wwwroot/你的站点域名。
+	  #以下命令都需要在站点目录进行执行。
+    read -p "请输入域名:" domain
+	cd /www/wwwroot/$domain
+	echo -e "${Info}:删除目录下文件" 
+    chattr -i .user.ini && rm -rf .htaccess 404.html index.html
+	echo -e "${Info}:下载v2broad到自己的网站目录" 
+	git clone -b master https://github.com/v2board/v2board.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard
+	echo -e "${Info}:下载composer并安装" 
+    wget https://getcomposer.org/download/1.9.0/composer.phar && php composer.phar install
+	echo -e "${Info}:正式安装v2broad，根据提示输入自己数据库的相应信息，共5项" 
+    php artisan v2board:install
+	 
+	echo -e "${Info}:网站配置
+	BTPanel面板 > 网站 > 点击网站 > 网站目录 
+	 取消防跨站攻击，运行目录选择/public，最后点击保存
+
+	BTPanel面板 > 网站 > 点击网站 > 伪静态  
+	 填入下面代码
+location /downloads {
+}
+
+location / {  
+    try_files $uri $uri/ /index.php$is_args$query_string;  
+}
+
+location ~ .*\.(js|css)?$
+{
+    expires      1h;
+    error_log off;
+    access_log /dev/null; 
+}"	 
+
+	echo -e "${Info}:配置定时任务和添加守护队列
+	BTPanel面板 > 计划任务 
+	 在 任务类型 选择“Shell脚本”
+	 在 任务名称 填写 任务名称
+	 在 执行周期 选择好执行时间周期
+	 在 脚本内容 栏填写以下代码
+	 */1 * * * * php /www/wwwroot/网站目录/artisan schedule:run
+	 
+	 在BTPanel面板 > 软件商店 找到安装的 PM2管理器 ，点击设置 安装下面所示添加对应项
+	 在 项目列表 选择路径“/www/wwwroot/网站目录/”
+	 pm2.yaml
+	 任务名称
+	  "
 
 
+	 
+	 
+	 ##配置站点目录及伪静态
+	  #添加完成后编辑添加的站点 > Site directory > Running directory 选择 /public 保存。
+	  #添加完成后编辑添加的站点 > URL rewrite 填入伪静态信息。(注意删除下面代码前的“#”号）	
+#location /downloads {
+#}
+
+#location / {  
+#    try_files $uri $uri/ /index.php$is_args$query_string;  
+#}
+
+#location ~ .*\.(js|css)?$
+#{
+#    expires      1h;
+#    error_log off;
+#    access_log /dev/null; 
+#}	
+
+	 ##配置定时任务
+	  #aaPanel 面板 > Cron。
+	  #在 Type of Task 选择 Shell Script
+	  #在 Name of Task 填写 v2board
+	  #在 Period 选择 N Minutes 1 Minute
+	  #在 Script content 填写 php /www/wwwroot/路径/artisan schedule:run
+	  #根据上述信息添加每1分钟执行一次的定时任务。
+
+	 ##启动队列服务
+	  #V2board的邮件系统强依赖队列服务，你想要使用邮件验证及群发邮件必须启动队列服务。下面以aaPanel中nodejs的PM2服务来守护队列服务作为演示。
+	  #aaPanel 面板 > App Store > Deployment
+	  #找到PM2 Manager 4.2.2进行安装，安装完成后按照如下填写
+	  #在 Project root directory 选择站点目录
+	  #在 Startup file name 填写 pm2.yaml
+	  #在 project name 填写 v2board
+	  #填写后点击Add添加即可运行。当然你也可以使用supervisor进行守护。
+	  #aaPanel在安装PM2的时候可能会造成问题无法安装，你可以手动进行PM2安装。如何安装可以参考Google
+
+	 ##常见问题
+	  #Q：500错误
+	  #A：检查站点根目录权限，递归755，保证目录有可写文件的权限，也有可能是Redis扩展没有安装或者Redis没有按照造成的。你可以通过查看storage/logs下的日志来排查错误或者开启debug模式。	
+}
+
+
+
+
+
+
+
+system_update(){
+	apt-get update
+    apt-get upgrade
+	reto_menu_exit
+}
+
+chang_time_zone(){
+	timedatectl set-timezone Asia/Shanghai
+	apt-get install -y ntp
+    systemctl enable ntp
+    systemctl restart ntp
+	reto_menu_exit
+}
 
 chang_ssh_password(){
 	passwd
@@ -92,29 +239,45 @@ chang_ssh_port(){
 	reto_menu_exit
 }
 
-chang_time_zone(){
-	timedatectl set-timezone Asia/Shanghai
-	apt-get install -y ntp
-    systemctl enable ntp
-    systemctl restart ntp
+Create_key_pair(){
+	cd .ssh
+	ssh-keygen -t rsa -b 4096  #-b 参数，指定了长度，也可以不加-b参数，直接使用ssh-keygen -t rsa，执行密钥生成命令，基本上是一路回车既可以了，但是需要注意的是：执行命令的过程中是会提示呢输入密钥文件名和密码，文件名建议用VPS的IP，密码要输入两次相同的进行确认，不需要密码直接回车就行。
+	cat id_rsa.pub >> authorized_keys
+	cd /etc/ssh/
+	cat RSAAuthentication yes >> /etc/ssh/sshd_config
+	cat PubkeyAuthentication yes >> /etc/ssh/sshd_config 
+#	cat "AuthorizedKeysFile .ssh/authorized_keys" >> /etc/ssh/sshd_config 
 	reto_menu_exit
 }
 
-system_update(){
-	apt-get update
-    apt-get upgrade
-	reto_menu_exit
+install_iptables_persistent(){
+	iptables -I FORWARD -p tcp --dport 25 -j DROP
+	iptables -I INPUT -p tcp --dport 25 -j DROP
+	iptables -I OUTPUT -p tcp --dport 25 -j DROP
+	echo -e "安装过程中会询问您是否要保存当前IPv4规则、IPv6规则，全选YES"
+	apt-get install -y iptables-persistent
+	echo -e "已在/etc目录下生成/iptables目录，并生成rules.v4文件用于存放IPv4规则，rules.v6文件用于存放IPv6规则。"
+    read -p "(是否测试生效情况：y/n):" yn
+		[[ -z "${yn}" ]] && yn="y"
+		if [[ ${yn} == [Yy] ]]; then
+				nc -vz smtp-relay.gmail.com 25
+			else
+			reto_menu_exit
+		fi
 }
+
+
+
 
 Routing_test(){
-	wget https://github.com/Aromalct/VPS-correlation/blob/main/VPS-sh/testrace2.sh
+#	wget https://github.com/Aromalct/VPS-correlation/blob/main/VPS-sh/testrace2.sh
 	bash testrace2.sh
 	reto_menu_exit
 }
 
 
 install_BBR(){
-	wget https://github.com/Aromalct/VPS-correlation/blob/main/VPS-sh/tcp.sh
+#	wget https://github.com/Aromalct/VPS-correlation/blob/main/VPS-sh/tcp.sh
 	bash tcp.sh
 	re_run_BBR
 }
@@ -169,8 +332,9 @@ install_acme(){
 				echo -e "正式从指定服务器申请证书"
 	            bash /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt --issue -d $domain --dns dns_cf --keylength ec-256 --force
 	            echo -e "安装（拷贝）证书"
-	            read -p "设置证书文件夹:" ssl_filename
-	            mkdir /etc/ssl/$ssl_filename
+#	            read -p "设置证书文件夹:" ssl_filename
+#	            mkdir /etc/ssl/$ssl_filename
+	            mkdir /etc/ssl/$domain
 	            bash /root/.acme.sh/acme.sh --install-cert -d $domain --ecc \
                         --fullchain-file /etc/ssl/$ssl_filename/$domain.crt \
                         --key-file /etc/ssl/$ssl_filename/$domain.key
@@ -199,7 +363,8 @@ install_V2_UI(){
 }
 
 install_X_UI(){
-    bash <(curl -Ls https://raw.githubusercontent.com/sprov065/x-ui/master/install.sh) 0.2.0
+    #bash <(curl -Ls https://raw.githubusercontent.com/sprov065/x-ui/master/install.sh)
+	bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
 	x-ui
 	
 }
